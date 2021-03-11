968dc9e013e7171ae31066a3e7b560db412a457c
Hefler <heflerdev@gmail.com>
2021-03-11 02:08:27 -0300

    Create: App.js

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BookList from './BookList';
+import BookForm from './BooksForm';
+
+const App = () => (
+  <>
+    <h1> BookList </h1>
+    <BookList />
+    <BookForm />
+  </>
+);
+
+export default App;

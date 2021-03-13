d44cef8de2644ef995501650806509b26bca6208
Hefler <heflerdev@gmail.com>
2021-03-13 00:33:57 -0300

    Update: Combine Reducers

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import booksReducer from '../reducers/books';
+import filterReducer from '../reducers/filters';
 
-const store = createStore(booksReducer);
+const rootReducer = combineReducers({
+  booksReducer,
+  filterReducer,
+});
+
+const store = createStore(rootReducer);
+
 export default store;
